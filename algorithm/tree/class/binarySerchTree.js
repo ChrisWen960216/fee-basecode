0aecddd7988e7f87232429902053a4115549a97b
“ChrisWen960216 <957638221@qq.com>
2017-08-25 13:51:10 +0800

    create bst

---
+++ algorithm/tree/class/binarySerchTree.js
@@ -0,0 +1,17 @@
+/* Created By ChrisWen
+ * 17/8/235
+ * BST --- 二叉搜索树，允许且仅允许左边叶子节点储存比父节点小、右边叶子节点储存比父节点大的值
+ *  遍历：先序遍历、中序遍历、后序遍历
+ */
+
+class binarySearchTree {
+    constructor() {
+        this.state = {
+            node: {
+                key: null,
+                left: null,
+                right: null
+            }
+        }
+    }
+}
---
+++ algorithm/tree/index.js
@@ -0,0 +1,4 @@
+/* Created By ChrisWen
+ * 17/8/25
+ * This page just running into node enviroment,require all Trees to create a example file;
+ */

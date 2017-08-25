1f164ce1e63060b2c4b107dc81dbe66f47e0c028
“ChrisWen960216 <957638221@qq.com>
2017-08-25 09:03:23 +0800

    create put/get/remove(key) Function in class valuePair

--- algorithm/dictionary/class/valuePair.js
+++ algorithm/dictionary/class/valuePair.js
@@ -11,19 +11,80 @@ module.exports = class valuePair extends HashTable {
         return super.loseloseHashCode(key);
     }
 
+    //添加
     put(key, value) {
         let position = valuePair.loseloseHashCode(key);
         let table = this.table;
+
         if (table[position] === undefined) {
             table[position] = new LinkedList();
         }
         table[position].append(new valuePair(key, value));
     }
 
+    //查找
     get(key) {
         let position = valuePair.loseloseHashCode(key);
         let table = this.table;
+
+        if (table[position] !== undefined) { //验证特定位置上是否有元素存在
+            let current = table[position].getHead(); //获得linkedlist头部
+            while (current.next) { //遍历链表找到值
+                if (current.next.element.key === key) {
+                    /** node链表中有element 和 next指针，同时element属性又是 class valuePair 的示例
+                     *  element:{
+                     * key:value}
+                     */
+                    return current.element.value;
+                }
+                current = current.next;
+            }
+
+            if (current.element.key === key) {
+                return current.element.value;
+            }
+        }
+        return undefined;
+    }
+
+    //删除
+    remove(key) {
+        let position = valuePair.loseloseHashCode(key);
+        let table = this.table;
+
         if (table[position] !== undefined) {
+            let current = table[position].getHead();
+            function removeKey(key) {
+                if (current.element.key === key) {
+                    table[position].remove(current.element);
+                    if (table[position].isEmpty()) {
+                        table[position] = undefined;
+                    }
+                    return true;
+                }
+            }
+            while (current.next) {
+                // if (current.element.key === key) {
+                //     table[position].remove(current.element);
+                //     if (table[position].isEmpty()) {
+                //         table[position] = undefined;
+                //     }
+                //     return true;
+                // }
+                this.removeKey(key);
+                current = current.next;
+            }
+            this.removeKey(key);
+
+        // if (current.element.key === key) {
+        //     table[position].remove(current.element);
+        //     if (table[position].isEmpty()) {
+        //         table[position] = undefined;
+        //     }
+        //     return true;
+        // }
+        } else {
+            return undefined
         }
     }
 }

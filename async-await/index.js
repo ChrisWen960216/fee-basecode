00d1839ac691ffff4e562fc31ad6c38e38fc3ae5
“ChrisWen960216 <957638221@qq.com>
2017-11-06 13:35:54 +0800

    Add basic closure

--- async-await/index.js
+++ async-await/index.js
@@ -1,24 +1,15 @@
 const sleep = time => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(console.log('resolve Promise'));
-        }, time);
-    })
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(console.log('resolve Promise'));
+    }, time);
+  });
 };
 
 const start = async () => {
-    console.log('start');
-    await sleep(3000);
-    console.log('end');
+  console.log('start');
+  await sleep(3000);
+  console.log('end');
 };
 
-start()
-
-// const startCount = async () => {
-//     for (let i = 0; i < 11; i++) {
-//         console.log(`当前是第${i}次等待`);
-//         await sleep(1000);
-//     }
-// }
-
-// startCount();
+start();
---
+++ closure/index.js
@@ -0,0 +1,35 @@
+/* Created By ChrisWen
+ * 17/11/06
+ * 闭包
+ */
+
+// 确切的说，这并不是闭包，而是闭包的一部分规则引用.
+function foo() {
+  const a = 2;
+  function bar() {
+    console.log(a);
+  }
+  bar();
+}
+
+foo();
+
+// 闭包
+function wait(message) {
+  setTimeout(function timer() {
+    console.log(message);
+  }, 1000);
+}
+
+wait('Hello World');
+// 只要使用了回调函数，实际上就是在使用闭包
+
+// for (var i = 1; i < 5;i++) {
+//   setTimeout(function timer() { console.log(i); }, i * 1000);
+// }
+
+for (var i = 1; i < 5;i++) {
+  (function (j) {
+    setTimeout(function timer() { console.log(j); }, j * 1000);
+  })(i);
+}

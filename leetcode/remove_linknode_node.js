bf7bea9c873595315b0685b062f41d82488d59bb
“ChrisWen960216 <957638221@qq.com>
2017-11-20 11:25:06 +0800

    Add insert function of linkNode

--- leetcode/remove_linknode_node.js
+++ leetcode/remove_linknode_node.js
@@ -34,11 +34,8 @@ function genNodes(num){
  * @return {LinkNode}
  */
 function remove(linknode,index){
-  let assitsLinkNode = linknode;
   let position = 0;
-  if(index === 0){
-    return linknode.next;
-  }
+  let assitsLinkNode = index === 0 ? linknode.next : linknode;
   while(index > 1 && position < index - 1) {
     if(assitsLinkNode.next === null || index < 0){
       return linknode
@@ -52,9 +49,50 @@ function remove(linknode,index){
   return linknode;
 }
 
+// 想到了一个绝妙的算法，可惜这里的地方太小，写不下...
+// 谁说写不下？！
+function remove(linknode, index) {
+  let i = 0, pr = index === 0 ? linknode = linknode.next : linknode;
+  while (pr && i < index){ (i++ === index - 1 && pr.next) ? pr.next = pr.next.next : pr = pr.next; }
+  return linknode;
+}
+
+/**
+ * 插入节点，插入在目标之后,非法情况则不变
+ * @param {LinkNode} linknode
+ * @param {_node} simple_linknode
+ * @param {number} index
+ * @return {LinkNode}
+ */
+function insert(linknode, _node, index){
+  let position = 0;
+  let assitsLinkNode = linknode;
+  let sertIndex = index +1;
+  if(sertIndex === 0){
+    _node.next = linknode;
+    linknode = _node;
+  } else if(sertIndex > 0){
+    while(position < sertIndex-1) {
+      if(!assitsLinkNode) {
+        return linknode;
+      }
+      assitsLinkNode = assitsLinkNode.next;
+      position ++;
+    }
+    if(assitsLinkNode !== null){
+      _node.next = assitsLinkNode.next;
+    assitsLinkNode.next = _node
+    }
+  }
+  return linknode;
+}
+
 
 //完成remove方法
 const linknode = genNodes(6);
 const result = remove(linknode,0);
+const newNode = new LinkNode(6);
+const newLinkNode = insert(linknode, newNode, 1);
 console.log(result.toString());
+console.log(newLinkNode.toString());
 //console.assert(result.toString()==='0,1,2,3,5')

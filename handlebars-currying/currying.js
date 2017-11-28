81bf3b4508b14a5530d9a7d30d42cc56977e7365
“ChrisWen960216 <957638221@qq.com>
2017-11-28 16:15:10 +0800

    Learn Currying functions

--- handlebars-currying/currying.js
+++ handlebars-currying/currying.js
@@ -1,21 +1,35 @@
-"use strict";
-/** Created By ChrisWen
-  * 17/11/23
-  * 柯里化
-  */
-function curry(data) {
-    var dataArray = [];
-    return function (datas) {
-        return dataArray;
-    };
+// function curry(array) {
+//   const resultArray = [];
+//   return function(array) {
+//     if (arguments) {
+//       for (const k in arguments) {
+//         resultArray.push(arguments[k]);
+//       }
+//     } else {
+//       console.log(resultArray);
+//     }
+//   };
+// }
+
+function curry(array, param) {
+  if (!Array.isArray(param)) { param = []; }
+  return function() {
+    const paraCollection = Array.prototype.slice.call(arguments);
+    // console.log(param, param.concat(paraCollection));
+    if (param.length !== param.concat(paraCollection).length) {
+      return curry(array, param.concat(paraCollection));
+    } else {
+      return array.apply(undefined, param.concat(paraCollection));
+    }
+  };
 }
-var abc = function (a, b, c) {
-    return [a, b, c];
+
+var _array = function() {
+  return [];
 };
-var curried = curry(abc);
-curried(1)(2)(3);
-// => [1, 2, 3]
-curried(1, 2)(3);
-// => [1, 2, 3]
-curried(1, 2, 3);
-// => [1, 2, 3]
+
+var curried = curry(_array);
+
+curried(1)(2, 999)(3)(4, 5, 6, 7, 8, 9, 10)(); // => [1,2,3]
+// curry(1, 2)(3); // => [1,2,3]
+// curry(1, 2, 3); // => [1,2,3]

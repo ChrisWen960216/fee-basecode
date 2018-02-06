6490af1d2d52583b3dc54045249b5d3bc3d917a4
ChrisWen960216 <chriswen96@163.com>
2018-02-06 13:20:38 +0800

    Add virttual DOM

--- virtualDOM/element.js
+++ virtualDOM/element.js
@@ -9,6 +9,24 @@ class Element {
     this.props = props;
     this.children = children;
   }
+
+  // 渲染dom
+  render() {
+    const { props, tagName, children = [] } = this;
+    const el = document.createElement(tagName);
+    // 设置Element属性
+    for (const propName in props) {
+      const propValue = props[propName];
+      el.setAttribute(propName, propValue);
+    }
+
+    children.forEach((child) => {
+      // 递归创建子节点
+      const childEl = (child instanceof Element) ? child.render() : document.createTextNode(child);
+      el.appendChild(childEl);
+    });
+    return el;
+  }
 }
 
 module.exports = Element;
--- virtualDOM/index.js
+++ virtualDOM/index.js
@@ -1,5 +1,3 @@
-const Element = require('./element');
-
 const el1 = new Element('li', { class: 'item' }, ['Item1']);
 const el2 = new Element('li', { class: 'item' }, ['Item2']);
 const el3 = new Element('li', { class: 'item' }, ['Item3']);
@@ -8,3 +6,6 @@ const el5 = new Element('li', { class: 'item' }, ['Item5']);
 const elArray = [el1, el2, el3, el4, el5];
 
 const ul = new Element('ul', {id: 'list'}, elArray);
+
+const ulRoot = ul.render();
+document.getElementById('root').appendChild(ulRoot);
